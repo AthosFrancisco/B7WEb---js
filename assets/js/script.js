07f375fb99d16c844fabd4c7f2a49757d820bcee
Athos Francisco Pereira do Carmo <athos.carmo@mcmmontagens.com.br>
2021-01-12 18:00:38 -0300

    Módulo 3 - Aula 1,2,3 - strings

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let nome = 'Athos Francisco Pereira do Carmo';
+
+let resultado;
+resultado = nome.length;
+resultado = nome.indexOf('Francisc');
+resultado = nome.slice(5, 20);
+resultado = nome.slice(5);
+resultado = nome.slice(-5);
+resultado = nome.slice(-17, -12);
+resultado = nome.substring(5, 20);
+resultado = nome.substring(5);
+resultado = nome.substr(5, 10);
+resultado = nome.substr(-5, 5);
+resultado = nome.replace('Athos', 'Miria');
+resultado = nome.toUpperCase();
+resultado = nome.toLowerCase();
+resultado = nome.trim();
+resultado = nome[3];
+resultado = nome.split(' ');
+
+console.log(resultado);
