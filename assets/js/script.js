25d89aa8f31e8565fc65e27dd6f85c4ff215c10b
Athos Francisco Pereira do Carmo <athos.carmo@mcmmontagens.com.br>
2021-01-13 16:38:08 -0300

    Módulo 3 - Aula 5,6 - arrays básico

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
-let n = 10.0254225621
-let n1 = '10.65'
+let lista = ['Palio', 'Siena', 'Uno', 'Estrada']
+let lista2 = [1,2.,3]
 
 let resultado
-resultado = n.toString()
-resultado = n.toFixed(3)
-resultado = parseFloat(n1) + 2
-resultado = parseInt(n1) + 2
+resultado = lista.toString()
+resultado = lista.join('/')
+resultado = lista.indexOf('Uno')
+resultado = lista.concat(lista2)
 
 console.log(resultado)
+
+lista.pop() // remove o último elemento
+console.log(lista)
+
+lista.shift() //remove o primeiro elemento
+console.log(lista)
+
+lista.push('Toro') //adiciona item no array
+console.log(lista)
+
+lista.splice(1,1) //exclui elementos do array
+console.log(lista)
+
+lista.sort()
+lista.reverse()
+console.log(lista)
