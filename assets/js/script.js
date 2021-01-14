4734419476e4495e1f0813ceb8956f0ec0eae20a
Athos Francisco Pereira do Carmo <athos.carmo@mcmmontagens.com.br>
2021-01-14 16:56:50 -0300

    Módulo 3 - Aula 9,10,11 - Date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
-let lista = [
-    {id: 1, nome: 'Athos', sobrenome: 'Francisco'},
-    {id: 2, nome: 'Pedro', sobrenome: 'Lira'},
-    {id: 3, nome: 'Alexia', sobrenome: 'Lucia'},
-]
+let d = new Date()
+let resultado
 
-let pessoa = lista.find(function(item){
-    return item.sobrenome == 'Lucia'
-})
+resultado = d.getFullYear()
+resultado = d.getMonth() // retorna o mês mas a contagem começa do 0
+resultado = d.getDay() //retorna o dia da semana, começando no domingo (0)
+resultado = d.getDate() // dia atual
+resultado = d.getHours() 
+resultado = d.getMinutes() 
+resultado = d.getSeconds() 
+resultado = d.getMilliseconds() 
+resultado = d.getTime() 
 
-pessoa = lista.findIndex(function(item){
-    return item.sobrenome == 'Lucia'
-})
+// d.setFullYear(2023)
+// d.setFullYear(d.getFullYear()+3)
+// resultado = d.setMonth(60)
+// resultado = d.setMonth(d.getMonth() + 60)
+// resultado = d.setDate(d.getDate()+50)
+// resultado = d.setHours(d.getHours() + 50) 
+// resultado = d.setMinutes(d.getMinutes() + 50) 
+// resultado = d.setSeconds(d.getSeconds() + 50) 
+// resultado = d.setMilliseconds(d.getMilliseconds() + 50)  
+resultado = d.setTime(d.getTime() + 50)
 
-console.log(pessoa)
+// console.log(resultado)
+console.log(d)
