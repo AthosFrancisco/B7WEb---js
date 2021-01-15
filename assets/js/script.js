b011a86ef3303ce73efd847e94e9af9ac6915ee2
Athos Francisco Pereira do Carmo <athos.carmo@mcmmontagens.com.br>
2021-01-15 16:19:33 -0300

    Revert "Módulo 3 - Aula 14 - setTimeout"
    This reverts commit 7c349d8002904055b4908a12367c18e3a6b0c676.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,62 @@
+function showHour(){
+    let date = new Date()
+    let h, m, s
+    h = date.getHours() < 10 ? '0'+date.getHours() : date.getHours()
+    m = date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes()
+    s = date.getSeconds() < 10 ? '0'+date.getSeconds() : date.getSeconds()
+
+    let demo = document.querySelector('#demo')
+    demo.innerHTML = `${h}:${m}:${s}`
+}
+
+function cronometro(){
+    let hour, minute, secound, millesecound
+    hour = document.querySelector('#hour')
+    minute = document.querySelector('#minute')
+    secound = document.querySelector('#secound')
+    millesecound = document.querySelector('#millesecound')
+
+    // console.log(hour, minute, secound, millesecound)
+
+    let h, m, s, mm
+    h = Number.parseInt(hour.innerText)
+    m = Number.parseInt(minute.innerText)
+    s = Number.parseInt(secound.innerText)
+    mm = Number.parseInt(millesecound.innerText)
+
+    let date = new Date(2020,1,1,h,m,s,mm)
+    date.setMilliseconds(date.getMilliseconds() + 100)
+
+    h = date.getHours() < 10 ? '0'+date.getHours() : date.getHours()
+    m = date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes()
+    s = date.getSeconds() < 10 ? '0'+date.getSeconds() : date.getSeconds()
+    mm = date.getMilliseconds() < 10 ? '0'+date.getMilliseconds() : date.getMilliseconds()
+
+    hour.innerText = h
+    minute.innerText = m
+    secound.innerText = s
+    millesecound.innerText = mm
+}
+
+function clearCronometro(){
+    document.querySelector('#hour').innerText = '00'
+    document.querySelector('#minute').innerText = '00'
+    document.querySelector('#secound').innerText = '00'
+    document.querySelector('#millesecound').innerText = '000'
+}
+
 let time
 
-function rodar(nome){
-    time = setTimeout(function(){
-        document.querySelector('#demo').innerText = `Rodou ${nome}` 
-    }, 2000, nome)
+
+function iniciar(){
+    // time = setInterval(showHour, 1*1000)
+    clearCronometro()
+    time = setInterval(cronometro, 1*100)
 }
 
 function parar(){
-    clearTimeout(time)
+    clearInterval(time)
 }
 
-document.querySelector('#init').addEventListener('click', rodar('Athos'))
+document.querySelector('#init').addEventListener('click', iniciar)
 document.querySelector('#finish').addEventListener('click', parar)
