32a6e5f0412feb75f3844136195f9a6951bd6e64
Athos Francisco Pereira do Carmo <athos.carmo@mcmmontagens.com.br>
2021-02-04 11:36:49 -0300

    Módulo 3 - Aula 25 - Promise

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,15 @@
-// 22 - Includes e repeat
-
-let receitaArray = ['ovo', 'farinha', 'trigo']
-let nome = 'Athos'
-
-console.log(receitaArray.includes('ovos'))
-console.log(nome.includes('O'))
-
-console.log(nome.repeat(10))
-
-// 23 Objeto: Key, Values e Entries
-
-let pessoa = {
-    nome: 'Athos',
-    sobrenome: 'Francisco',
-    idade: 100
+const fazer = () =>{
+    return new Promise((res, rej) => {
+        // res('OK');
+        setTimeout(() => {
+            res('OK');
+        }, 3000);
+    })
 }
 
-console.log(Object.keys(receitaArray))
-console.log(Object.keys(pessoa))
+fazer()
+    .then((resposta) => {
+        console.log(resposta)
+    })
 
-console.log(Object.values(receitaArray))
-console.log(Object.values(pessoa))
-
-console.log(Object.entries(receitaArray))
-console.log(Object.entries(pessoa))
-
-// 24 - String, padStart, padEnd
-
-let cartao = '1234123412341234'
-
-let cartaoReduzido = cartao.slice(-4)
-let cartaoReduzidoFinal = cartao.slice(0, 4)
-
-console.log(cartaoReduzido.padStart(16, '*'))
-console.log(cartaoReduzidoFinal.padEnd(16, '*'))
+console.log('Depois da promessa')
