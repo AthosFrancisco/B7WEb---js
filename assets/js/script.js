79decf4e21c64b51975f3605e4d62772ca7859f4
Athos Francisco Pereira do Carmo <athos.carmo@mcmmontagens.com.br>
2021-02-04 13:59:40 -0300

    Módulo 3 - Aula 27 - async/await

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
-fetch('https://alunos.b7web.com.br/api/ping',
-    {
-        // method: 'POST', chamada post não funciona para essa url, por isso não dá pra enviar no bory da requisição
-        method: 'GET',
-        // body: JSON.stringify({
-        //     nome: 'Athos',
-        //     idade: 26
-        // })
-    })
-    .then((r)=>r.json())
-    .then((json) => {
-        console.log(json.resultado[0])
-    })
+const requisitar = () => {
+    fetch('https://alunos.b7web.com.br/api/ping')
+        .then((r) => r.json())
+        .then((json) => {
+            console.log(json.resultado[0])
+        })
+    
+        console.log('Depois de requisitar')
+}
+
+const requisitar2 = async () => {
+    
+    const r = await fetch('https://alunos.b7web.com.br/api/ping')
+        
+    const json = await r.json()
+    
+    console.log(json.resultado[0])
+    
+    console.log('Depois de requisitar 2')
+}
+
+requisitar2()
